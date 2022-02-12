681385ec315a54e4a8f481a555091e8b7e61f375
Yeonggyeong <jeljelkim129@gmail.com>
2022-02-12 22:32:13 +0900

    hanoi solution

--- Algorithms/baekjoon/Silver2_1914_하노이탑/solution.py
+++ Algorithms/baekjoon/Silver2_1914_하노이탑/solution.py
@@ -1,12 +1,11 @@
 def hanoi(start, median, end, n):
 
     if n == 1:
-        print(f'{start} {end}')
-        return 
-    
+        print(start, end, sep = " ")
+        return
     hanoi(start, end, median, n-1)
-    print(f'{start} {end}')
-    hanoi(median, end, start, n-1)
+    hanoi(start, median, end, 1)
+    hanoi(median, start, end, n-1)
 
 
 n = int(input())

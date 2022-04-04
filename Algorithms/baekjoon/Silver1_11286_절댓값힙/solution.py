095a8bbe7f729977cf95b206a5a067faaf6c0329
Yeonggyeong <jeljelkim129@gmail.com>
2022-04-04 13:32:11 +0900

    b9935 b11286 b11399

---
+++ Algorithms/baekjoon/Silver1_11286_절댓값힙/solution.py
@@ -0,0 +1,17 @@
+import heapq
+
+N = int(input())
+orders = [int(input()) for _ in range(N)]
+
+
+heap = []
+
+for i in range(N):
+    if orders[i] == 0:
+        if heap:
+            num = heapq.heappop(heap)
+            print(num[1])
+        else:
+            print(0)
+    else:
+        heapq.heappush(heap, [abs(orders[i]), orders[i]])
